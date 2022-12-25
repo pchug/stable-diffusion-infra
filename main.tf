4a233e1552d931386cab03cefdbe82514ddc0f23
Piyush Chugh <piyush.chugh@gmail.com>
2022-12-24 22:26:18 -0700

    vanilla

--- main.tf
+++ main.tf
@@ -13,11 +13,11 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-0cdbd57e40d4eec18"
-  instance_type = "t2.small"
+# resource "aws_instance" "app_server" {
+#   ami           = "ami-0cdbd57e40d4eec18"
+#   instance_type = "t2.small"
 
-  tags = {
-    Name = "stable-diffusion-server0"
-  }
-}
+#   tags = {
+#     Name = "stable-diffusion-server0"
+#   }
+# }
