4d89a83bb61cd93a2f6b376177073473396ec2a1
Piyush Chugh <piyush.chugh@gmail.com>
2022-12-24 22:25:20 -0700

    bump

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
 resource "aws_instance" "app_server" {
   ami           = "ami-0cdbd57e40d4eec18"
-  instance_type = "t2.micro"
+  instance_type = "t2.small"
 
   tags = {
     Name = "stable-diffusion-server0"
